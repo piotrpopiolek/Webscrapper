f8c4e6da376f372ced49321bb335345720e7465e
Arkadiusz Kolodynski <arekkolodynski@poczta.fm>
2018-04-06 22:38:18 +0200

    Add values scrapping

--- src/main/java/pl/pt/put/poznan/webscrapper/Crawler.java
+++ src/main/java/pl/pt/put/poznan/webscrapper/Crawler.java
@@ -3,7 +3,10 @@ package pl.pt.put.poznan.webscrapper;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -16,6 +19,16 @@ public class Crawler {
         String source = driver.getPageSource();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("source.html"))) {
             writer.write(source);
+        }
+        List<WebElement> elements;
+        elements = driver.findElements(By.xpath("//table[@class='table currency-table__table']/tbody/tr"));
+        if (!elements.isEmpty()) {
+            elements.stream().forEach((e) -> {
+                System.out.println(e.getText() + "  ");
+            });
+        } else {
+            driver.quit();
+            throw new IOException("Currencies not found!");
         }
         driver.quit();
     }
