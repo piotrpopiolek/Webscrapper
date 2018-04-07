32649c99b5651d80d4cdddfbf136180aaa8a1cf0
Arkadiusz Kolodynski <arekkolodynski@poczta.fm>
2018-04-07 21:54:33 +0200

    Added ability to extend crawler with new sites

--- src/main/java/pl/pt/put/poznan/webscraper/Crawler.java
+++ src/main/java/pl/pt/put/poznan/webscraper/Crawler.java
@@ -1,37 +1,28 @@
 package pl.pt.put.poznan.webscraper;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.util.List;
+import java.io.File;
 import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import com.machinepublishers.jbrowserdriver.JBrowserDriver;
 
 public class Crawler {
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws Exception {
+        String packageName = Crawler.class.getPackage().getName();
+        File dir = new File("src/main/java/" + packageName.replace(".", "/"));
+        File[] classesArray = dir.listFiles();
+        
+        for (File e : classesArray) {
+            String name = e.getName();
+            if (!name.equals("Crawler.java")) {
+                Class<?> c = Class.forName(packageName + "." + name.substring(0, name.length() - 5));
+                c.getConstructor().newInstance();
+            }
+        }
+    }
+    
+    public static WebDriver config() {
         WebDriver driver = new JBrowserDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    
-        driver.get("https://bitbay.net/pl/kurs-walut");
-        String source = driver.getPageSource();
-        try (Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("source.html"), "UTF-8"))) {
-            out.write(source);
-        }
-        List<WebElement> elements;
-        elements = driver.findElements(By.xpath("//table[@class='table currency-table__table']/tbody/tr"));
-        if (!elements.isEmpty()) {
-            elements.stream().forEach((e) -> {
-                System.out.println(e.getText() + "  ");
-            });
-        } else {
-            driver.quit();
-            throw new IOException("Currencies not found!");
-        }
-        driver.quit();
+        return driver;
     }
 }
