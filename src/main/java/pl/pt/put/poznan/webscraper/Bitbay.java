dd3bfc546bf4d5f22218e48a73c129e8278dc710
Piotr <piotr.popiolek93@gmail.com>
2018-04-09 11:18:03 +0200

    Klasa Coinmarketcap musi startować tylko raz na 24h.

--- src/main/java/pl/pt/put/poznan/webscraper/Bitbay.java
+++ src/main/java/pl/pt/put/poznan/webscraper/Bitbay.java
@@ -1,5 +1,7 @@
 package pl.pt.put.poznan.webscraper;
 
+
+
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -20,7 +22,6 @@ public class Bitbay {
         if (!elements.isEmpty()) {
             elements.stream().forEach((e) -> {
                 String position = e.getText();
-                System.out.println(position);
                 String[] splited = position.split("\\s+");
                 System.out.println(splited[1]);
                 System.out.println(splited[2]);
@@ -28,26 +29,34 @@ public class Bitbay {
                 System.out.println(splited[5]);
                 System.out.println(splited[6]);
                 System.out.println(splited[7]);
-//                if(splited[1].equals("PLN"))
-//                {
-//                CurrencyValue currencyvalue = new CurrencyValue("BTC");
-//                currencyvalue.setPriceInDollars(Double.parseDouble(splited[2]));
-//                currencyvalue.setBid(Double.parseDouble(splited[4]));
-//                currencyvalue.setAsk(Double.parseDouble(splited[5]));
-//                currencyvalue.setVolume(Double.parseDouble(splited[6]));
-//                CurrencyManagement management = CurrencyManagement.getInstance();
-//                management.addCurrencyValue(currencyvalue);
-//                }
-//                else
-//                {
-//                CurrencyValue currencyvalue = new CurrencyValue(splited[1]);
-//                currencyvalue.setPriceInDollars(Double.parseDouble(splited[2]));
-//                currencyvalue.setBid(Double.parseDouble(splited[4]));
-//                currencyvalue.setAsk(Double.parseDouble(splited[5]));
-//                currencyvalue.setVolume(Double.parseDouble(splited[6]));
-//                CurrencyManagement management = CurrencyManagement.getInstance();
-//                management.addCurrencyValue(currencyvalue);
-//                }       
+                if(splited[1].equals("PLN"))
+                {
+                CurrencyValue currencyvalue = new CurrencyValue("BTC");
+                currencyvalue.setPriceInBitcoin(1.0);
+                currencyvalue.setBid(Double.parseDouble(splited[4]));
+                currencyvalue.setAsk(Double.parseDouble(splited[5]));
+                currencyvalue.setVolume(Double.parseDouble(splited[6]));
+                CurrencyManagement management = CurrencyManagement.getInstance();
+                management.addCurrencyValue(currencyvalue);
+                }
+                else if(splited[1].equals("EUR"))
+                {
+                
+                }
+                else if(splited[1].equals("USD"))
+                {
+                
+                }
+                else
+                {
+                CurrencyValue currencyvalue = new CurrencyValue(splited[7]);
+                currencyvalue.setPriceInBitcoin(Double.parseDouble(splited[2]));
+                currencyvalue.setBid(Double.parseDouble(splited[4]));
+                currencyvalue.setAsk(Double.parseDouble(splited[5]));
+                currencyvalue.setVolume(Double.parseDouble(splited[6]));
+                CurrencyManagement management = CurrencyManagement.getInstance();
+                management.addCurrencyValue(currencyvalue);
+                }       
             });
         } else {
             driver.quit();
