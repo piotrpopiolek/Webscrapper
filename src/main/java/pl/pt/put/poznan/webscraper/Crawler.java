f93474f76ffc817a46541de1d0d7ddba18b61820
Arkadiusz Kołodyński <arekkolodynski@poczta.fm>
2018-05-01 15:37:14 +0200

    thread moved to func

--- src/main/java/pl/pt/put/poznan/webscraper/Crawler.java
+++ src/main/java/pl/pt/put/poznan/webscraper/Crawler.java
@@ -5,22 +5,35 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import com.machinepublishers.jbrowserdriver.JBrowserDriver;
 import java.lang.reflect.InvocationTargetException;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Crawler {
-    
+
     public static void main(String args[]) throws Exception {
-        WebDriver driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+        WebDriver driver = new JBrowserDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         String packageName = Crawler.class.getPackage().getName();
         File dir = new File("src/main/java/" + packageName.replace(".", "/"));
         File[] listOfClasses = dir.listFiles();
-        
-        Thread t = new Thread() { 
+
+        Thread t = runThread(driver, listOfClasses, packageName);
+
+        t.start();
+        System.out.println("\nPress ENTER to terminate...\n");
+        System.in.read();
+        t.stop();
+
+        driver.quit();
+    }
+
+    static Thread runThread(WebDriver driver, File[] listOfClasses, String packageName) {
+        Thread t;
+        t = new Thread() {
             @Override
             public void run() {
                 try {
-                    while(true) {
+                    while (true) {
                         for (File e : listOfClasses) {
                             String name = e.getName();
                             if (!name.equalsIgnoreCase(Crawler.class.getSimpleName().concat(".java"))) {
@@ -28,19 +41,12 @@ public class Crawler {
                                 c.getConstructor(WebDriver.class).newInstance(driver);
                             }
                         }
-                        Thread.sleep(1000);
                     }
-                } catch (ClassNotFoundException | IllegalAccessException | IllegalArgumentException | InstantiationException | InterruptedException | NoSuchMethodException | SecurityException | InvocationTargetException e) {
-                    System.out.println(e.getMessage());
+                } catch (ClassNotFoundException | IllegalAccessException | IllegalArgumentException | InstantiationException | NoSuchMethodException | SecurityException | InvocationTargetException e) {
+                    Logger.getLogger(Crawler.class.getName()).log(Level.SEVERE, null, e);
                 }
-            } 
+            }
         };
-        
-        t.start();
-        System.out.println("\nPress ENTER to terminate...\n");
-        System.in.read();
-        t.stop();
-        
-        driver.quit();
+        return t;
     }
 }
