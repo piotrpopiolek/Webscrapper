1f577d11bf37f38cd47fd8279ab7b8219ac88424
Arkadiusz Kołodyński <arekkolodynski@poczta.fm>
2018-05-04 00:22:23 +0200

    multithreading

--- src/main/java/pl/pt/put/poznan/webscraper/Crawler.java
+++ src/main/java/pl/pt/put/poznan/webscraper/Crawler.java
@@ -4,49 +4,54 @@ import java.io.File;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import com.machinepublishers.jbrowserdriver.JBrowserDriver;
+import com.machinepublishers.jbrowserdriver.Settings;
+import com.machinepublishers.jbrowserdriver.UserAgent;
 import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Crawler {
 
     public static void main(String args[]) throws Exception {
-        WebDriver driver = new JBrowserDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         String packageName = Crawler.class.getPackage().getName();
         File dir = new File("src/main/java/" + packageName.replace(".", "/"));
         File[] listOfClasses = dir.listFiles();
 
-        Thread t = runThread(driver, listOfClasses, packageName);
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(listOfClasses.length - 1);
 
-        t.start();
-        System.out.println("\nPress ENTER to terminate...\n");
+        int initialDelay = 0;
+        for (File e : listOfClasses) {
+            String name = e.getName();
+            if (!name.equalsIgnoreCase(Crawler.class.getSimpleName().concat(".java"))) {
+                executor.scheduleAtFixedRate(runConst(packageName + "." + name.replace(".java", "")), initialDelay, 60, TimeUnit.SECONDS);
+                initialDelay = initialDelay + 10;
+            }
+        }
+        
+        System.out.println("\nPress ENTER to terminate...");
         System.in.read();
-        t.stop();
-
-        driver.quit();
+        System.out.println("Waiting for termination...");
+        
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.MINUTES);
+        System.exit(0);
     }
 
-    static private Thread runThread(WebDriver driver, File[] listOfClasses, String packageName) {
-        Thread t;
-        t = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    while (true) {
-                        for (File e : listOfClasses) {
-                            String name = e.getName();
-                            if (!name.equalsIgnoreCase(Crawler.class.getSimpleName().concat(".java"))) {
-                                Class<?> c = Class.forName(packageName + "." + name.replace(".java", ""));
-                                c.getConstructor(WebDriver.class).newInstance(driver);
-                            }
-                        }
-                    }
-                } catch (ClassNotFoundException | IllegalAccessException | IllegalArgumentException | InstantiationException | NoSuchMethodException | SecurityException | InvocationTargetException e) {
-                    Logger.getLogger(Crawler.class.getName()).log(Level.SEVERE, null, e);
-                }
+    private static Runnable runConst(String className) {
+        Runnable r = () -> {
+            System.out.println("\n" + Thread.currentThread().getName() + "\n");
+            WebDriver driver = new JBrowserDriver(Settings.builder().userAgent(UserAgent.CHROME).build());
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            try {
+                Class<?> c = Class.forName(className);
+                c.getConstructor(WebDriver.class).newInstance(driver);
+            } catch (ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                Logger.getLogger(Crawler.class.getName()).log(Level.SEVERE, null, ex);
             }
+            driver.quit();
         };
-        return t;
+        return r;
     }
 }
